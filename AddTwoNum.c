9a9ba157c5e2dfef32af8b8031e5bc76d7ffb009
Prabu M <43335779+prabuvimal2000@users.noreply.github.com>
2020-07-28 20:57:03 +0530

    Create AddTwoNum.c

---
+++ AddTwoNum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int num1,num2,sum;
+  printf("Enter the first num : ");
+  scanf("%d",&num1);
+  printf("Enter the second num : ");
+  scanf("%d",&num2);
+  sum=num1+num2;
+  printf("Enterted number sum is :\n %d+%d= %d",num1,num2,sum);
+  return 0;
+}

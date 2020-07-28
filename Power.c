303135528305b2d97813f32fb8126c8aa1f763ed
Prabu M <43335779+prabuvimal2000@users.noreply.github.com>
2020-07-28 21:39:33 +0530

    Create Power.c

---
+++ Power.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+ int num,power;
+ printf("Enter the number: ");
+ scanf("%d",&num);
+ printf("Entered the power ")
+ scanf("%d",&power)
+ printf("The given power of %d is :%d",num,pow(num,power));
+ return 0;
+}

606bdf4c5d064250e30ee67c7fd7608b66b58f58
Prabu M <43335779+prabuvimal2000@users.noreply.github.com>
2020-07-29 18:22:10 +0530

    Create String.c

---
+++ String.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+char name[]={};
+scanf("%s",name);
+printf("Hi Mr.%s",name);
+/*here we also use gets(name) 
+and for print use puts(name) also*/
+return 0;
+}
